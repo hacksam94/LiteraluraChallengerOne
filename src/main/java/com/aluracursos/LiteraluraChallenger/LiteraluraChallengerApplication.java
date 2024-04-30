7ea1b5e7e198d1aefb32fdeabe7c0627b5774e29
Samir Brun <hacksam94@gmail.com>
2024-04-29 21:05:29 -0500

    mapeando datos , y mostrando la informacion

--- src/main/java/com/aluracursos/LiteraluraChallenger/LiteraluraChallengerApplication.java
+++ src/main/java/com/aluracursos/LiteraluraChallenger/LiteraluraChallengerApplication.java
@@ -21,10 +21,10 @@ public class LiteraluraChallengerApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
        var consumoApi = new ConsumoApi() ;
-		String url;
+
 		var json = consumoApi.obtenerDatos( "https://gutendex.com/books/" )  ;
 
-		//System.out.println("json = " + json) ;    //ver lo que esta entregando el api en json
+		//System.out.println("json = " + json) ;    //ver lo que está entregando el api en json
 
 		DeserializarDatos  deserializar = new DeserializarDatos() ;
         var datos = deserializar.obtenerDatos( json , DatosLibros.class);
--- src/main/java/com/aluracursos/LiteraluraChallenger/service/IDeserializarDatos.java
+++ src/main/java/com/aluracursos/LiteraluraChallenger/service/IDeserializarDatos.java
@@ -3,3 +3,4 @@ package com.aluracursos.LiteraluraChallenger.service;
 public interface IDeserializarDatos {
     <T>T obtenerDatos(String json , Class <T> clase) ;
 }
+//
